44b70e13910278d6f841a4038e873bf2a265f66c
hanrui <rui19921122@qq.com>
2016-03-16 12:18:53 +0800

    commit by accident

--- accidentCase/models.py
+++ accidentCase/models.py
@@ -5,7 +5,6 @@ from django.core.exceptions import PermissionDenied
 
 # Create your models here.
 class Accident(models.Model):
-    name = models.CharField(max_length=50)
     publish_person = models.ForeignKey(User)
     publish_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
